625abeb4c5fd377ee7a2a2fd3344d7fd5d6b01b3
Drguezpro <rodriguez.ottonello@gmail.com>
2023-10-08 17:05:08 -0300

    Ejercicios de Javascrip con funciones

---
+++ M03 JavaScript Fundamentos/Probando.js
@@ -0,0 +1,11 @@
+function sonIguales(x, y) {
+    // Retorna true si "x" e "y" son iguales.
+    // De lo contrario, retorna false.
+    // Tu código:
+    if (x == y) {
+       console.log (true);
+    } else {
+       console.log (false)
+    }
+ }
+ 
